e417d9dda8d9a1d3914e134add2b8421d5ec5051
GitCacaTyu <carlosalbertolacerdadias@gmail.com>
2025-07-31 15:14:43 -0300

    Botôes

---
+++ ipt.js
@@ -0,0 +1,78 @@
+// código JS do jogo dos botões aqui (mantido conforme pedido)
+
+
+document.querySelector('#botao').addEventListener('click' , () => {
+alert("ahn! você clicou no botão * 8 =  zinho seu zé da manga!")
+});
+document.querySelector('#oi').addEventListener('click' , () => {
+    alert("Parabéns você é muito corajoso  ")
+    });
+
+    document.querySelector('#po').addEventListener('click' , () => {
+        alert("Bem-Vindo  ")
+        });
+ 
+        document.querySelector('#p').addEventListener('click' , () => {
+            alert(" uva  tem mel e abacaxiG  tem  pãoR")
+            });
+
+
+
+    document.querySelector('#bo').addEventListener('click' , () => {
+        alert("você tá pronto 128")
+        });
+ 
+
+        document.querySelector('#tao').addEventListener('click' , () => {
+            alert("muito ???????? por ?????")
+            });
+
+
+            document.querySelector('#otao').addEventListener('click' , () => {
+                alert("descobrir a palavra de cima boa sorte")
+                });
+
+                document.querySelector('#ta').addEventListener('click' , () => {
+                    alert(" a jornada começou")
+                    });
+
+                    document.querySelector('#a').addEventListener('click' , () => {
+                        alert("o que vocêA tá fazendoB aqui?")
+                        });
+
+                        
+                        document.querySelector('#e').addEventListener('click' , () => {
+                            alert("três tampados Aforam  promoçãoI")
+                            });
+
+                            
+                            document.querySelector('#t').addEventListener('click' , () => {
+                                alert(" eu J já falei não O ")
+
+                                });
+
+                                  
+                            document.querySelector('#o').addEventListener('click' , () => {
+                                alert("vc tem problema! ai tá bom 0800 ")
+
+                                });
+
+                                  
+                            document.querySelector('#d').addEventListener('click' , () => {
+                                alert(" 8 descobre vc passou dela né kkkkk ")
+
+                                });
+
+                                  
+                            document.querySelector('#n').addEventListener('click' , () => {
+                                alert(" nossa ,para já deu O")
+
+                                });
+
+
+                                  
+                            document.querySelector('#r').addEventListener('click' , () => {
+                                alert(" D ´´´´´´ R as letras para parar de vez logo")
+                                          
+                                });
+            
