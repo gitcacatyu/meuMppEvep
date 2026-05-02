16ebead178dbd9a57f0167aabee708ae4be683b6
GitCacaTyu <carlosalbertolacerdadias@gmail.com>
2026-05-02 15:41:29 -0300

    version 8

---
+++ j12.js
@@ -0,0 +1,12 @@
+function perfil(){
+    window.location.href = "pe.html";
+}
+function enme(){
+    window.location.href = "mel.html";
+}
+function irLoja(){
+    window.location.href = 'lo.html'
+}
+function Irjogo(){
+    window.location.href = 'j.html'
+}
